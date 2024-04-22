6a4a5594c3c05a141c0c59238a39d34c453d880e
Lyall <lyall@iriesoft.net>
2024-04-22 02:34:51 +0200

    checkpoint

--- src/vue-vif/components/Ace.vue
+++ src/vue-vif/components/Ace.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
 
-import { computed, reactive, ref } from "vue"
-import { apply } from "json-logic-js"
-// import { flatten } from 'flatten-anything'
+import { computed, reactive, ref, watch } from "vue"
+import { flatten, restructure } from "../composables/useVeeIfHelper";
+import type { ISchema } from "../typings";
+import { getByPath, setByPath } from 'dot-path-value';
+import { apply } from "json-logic-js";
 
-const data = ref({});
-
-interface ISchema {
-	component?: any;
-	children?: ISchema[];
-	class?: string;
-	type?: string,
-	name?: string,
-	label?: string,
-	validation?: string;
-	vif?: any;
-	visible?: boolean;
-}
-
-
-const schema: ISchema[] = [
+const data = ref<any>({});
+const schema = ref<ISchema[]>([
 	{
 		type: "text",
 		name: "firstname",
@@ -48,16 +36,10 @@ const schema: ISchema[] = [
 		name: "personalDetails",
 		children: [
 			{
-				type: "group",
-				name: "extraDetails",
-				children: [
-					{
-						type: "text",
-						name: "age2",
-						label: "How old are you 2?",
-						vif: { "==": [{ "var": "lastname" }, "b"] },
-					},
-				]
+				type: "text",
+				name: "age2",
+				label: "How old are you 2?",
+				vif: { "==": [{ "var": "lastname" }, "b"] },
 			},
 		]
 	},
@@ -73,273 +55,68 @@ const schema: ISchema[] = [
 		label: "Star me2",
 		vif: { "==": [{ "var": "personalDetails[0].age2" }, "55"] },
 	},
-];
+]);
 
+const flattened = computed(() => {
+	return flatten(data.value, schema.value)
+});
 
-/*
-schema.forEach(object => {
-
-
-	if (object.vif) {
-		const rule = object.vif;
-		const result = apply(
-			rule,
-			data.value
-		);
-		object.visible = result;
-	}
-
-	object.children?.forEach(item => {
-		if (item.vif) {
-			const rule = item.vif;
-			const result = apply(
-				rule,
-				data.value
-			);
-			item.visible = result;
-		};
-
-	})
-})
-*/
-
-
-const veeIf = ((item: any) => {
-
-
-	if (item.vif) {
-		const rule = item.vif;
-		const result = apply(
-			rule,
-			data.value
-		);
-		item.visible = result;
-	}
-
-	// if (item.children) return item.children.map(m => veeIf(m));
-
-	// if (item.children) return item.children.forEach(m => veeIf(m));
-
-	// if(!item.vif) item.visible = false;
-
-	return item;
-
+const restructured = computed(() => {
+	return restructure(flattened.value.filter(f => f.visible !== false))
 });
 
 
-let currentId = 0;
+const dotpath = (path: string, expectedvalue: any): boolean => {
 
-function flat(array: any[], parentId: number | null = null) {
-	var result: any[] = [];
-	array.forEach(function (a) {
-		// result.push(a);
+	//const rule = `{ "${condition}": [{ "var": "${path}" }, "${expectedvalue}"] }`;
 
+	//const r = JSON.parse(rule);
 
-		const rule = a.vif;
-		const jsonLogicResult = apply(
-			rule,
-			data.value
-		);
-		a.visible = jsonLogicResult;
+	//const value = apply(r, data);
 
-		result.push({
-			...a,
-			id: ++currentId,
-			parent: parentId
-		});
+	const value = getByPath(data.value, path);
 
-		if (Array.isArray(a.children)) {
-			result = result.concat(flat(a.children, currentId));
-		}
-	});
-	return result;
+	// console.log("a", typeof value);
+
+	return value === expectedvalue;
 }
 
-const out2 = flat(schema);
-console.log(out2);
+const jsonLogic = (rule: object): boolean => {
 
-// **********************************************************
-// for loop
-// **********************************************************
+	const value = apply(rule, data.value);
 
-/*
-let result: any[] = [];
+	if ("boolean" === typeof value) return value;
 
-var _flatten = flatten(schema, ['children']);
-console.log(_flatten);
-
-const loeya = (arr: any[], level: number = 0, parent: null | number = null) => {
-
-	// let currentLevel = level || 0;
-
-	for (let i = 0; i < arr.length; i++) {
-		const item = arr[i];
-
-		const id = result.length;
-
-		// create a disconnected object
-		const obj = {
-			...item,
-			id: id,
-			parent: parent,
-			level: level
-		};
-
-		// delete the children key, so its not address back in the temp object array
-		delete obj["children"];
-
-		// add object to our store array (without the children)
-		result.push(obj);
-
-		// check for children
-		if (item.children && item.children.length) {
-			++level;
-			loeya(item.children, level, id);
-		}
-
-		// result.push(obj);
-	}
-
-	// remove all children keys
-	// delete obj["children"];	
-	// result.forEach((r) => delete r["children"]);
-
-	return result;
+	return false;
 }
-
-let listing = loeya(schema);
-
-const g = groupBy(listing, (c) => c.level);
-
-console.log(g);
-*/
-
-// *****************************************************************
-// *****************************************************************
-
-/*
-// store the keys
-let keys: string[] = [];
-
-Object.keys(listing[0]).forEach((j) => {
-	keys.push(j);
-});
-*/
-
-
-// **********************************************************
-
-
-/*const computedSchema = computed(() => {
-
-
-	const parsed = schema.flatMap((s) => {
-		return veeIf(s);
-	});
-
-
-	return parsed.filter(f => {
-		return f.visible !== false
-	});
-
-	// return parsed;
-
-	// veeIf(schema);
-
-
-
-	// return schema.filter(f => f.visible === true);
-
-});*/
-
-/*
-
-
-function veeIf(item: any): any {
-
-	if (item.children && Array.isArray(item.children)) {
-		return item.children.map((m: any) => { return veeIf(m) });
-	}
-
-	if (item.vif) {
-		const rule = item.vif;
-		const result = apply(
-			rule,
-			data.value
-		);
-		item.visible = result;
-	}
-
-	return item;
-}
-
-const parsedSchema = computed(() => {
-	return schema.map((value: any) => {
-		return veeIf(value);
-		//return value;
-	});
-});
-
-const computedSchema = computed(() => {
-	return parsedSchema.value.filter((f) => f.visible !== false);
-});
-
-
-const d = reactive({
-	list: [1, 2, 3],
-})
-
-*/
-
-const computedSchema = computed(() => {
-
-	/*const filtered = filterNested({
-	  array: schema,
-	  search: null,
-	  searchAttribute: 'visible',
-	  childrenPath: 'children',
-	})*/
-
-
-	// return listing;
-	//return listing.filter((f) => f.visible !== false);\\
-
-	return [];
-
-});
-
-const reconstructedSchema = computed(() => {
-	return [];
-});
-
-function groupBy(xs, f) {
-	return xs.reduce((r, v, i, a, k = f(v)) => ((r[k] || (r[k] = [])).push(v), r), {});
-}
-
 
 </script>
 
 <template>
 	<div>
 
+		<FormulateForm v-model="data" v-auto-animate>
+
+			<FormulateInput type="text" name="firstname" label="First name X" />
+			<FormulateInput type="checkbox" name="available" label="Are you available?" v-if="data.firstname === 'a'" />
+			<FormulateInput type="text" name="middlename" label="Middle name" v-if="data.available === true" />
+			<FormulateInput type="text" name="lastname" label="Last name" v-if="data.firstname === 'a'" />
+
+			<FormulateInput type="group" name="personalDetails">
+				<FormulateInput type="group" name="extraDetails">
+					<FormulateInput type="text" name="age2" label="Age 2" v-if="data.lastname === 'b'" />
+				</FormulateInput>
+			</FormulateInput>
 
 
-		<hr />
-		<hr />
-
-		<FormulateForm v-model="data" :schema="computedSchema" v-auto-animate />
-
+			<FormulateInput type="text" name="star1" label="Star 1"
+				v-if="dotpath('personalDetails.0.extraDetails.0.age2', '55')" />
+			<FormulateInput type="text" name="star2" label="Star 2" v-if="jsonLogic({ '==': [{ 'var': 'personalDetails.0.extraDetails.0.age2' }, '55'] })" />
 
 
-		<pre class="double-row">{{ data }}</pre>
+		</FormulateForm>
 
-		<pre>{{ computedSchema }}</pre>
-
-
-		<vue2-simple-datatable :data="out2" :columns="['name', 'label', 'visible', 'id', 'level', 'parent']" />
-
-		<pre>{{ reconstructedSchema }}</pre>
-
+		<pre>{{ data }}</pre>
 
 	</div>
 </template>
