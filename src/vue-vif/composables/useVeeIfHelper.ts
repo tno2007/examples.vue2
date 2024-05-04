bedd88ebef639d4abfbe573fe8e65bae91894eed
Lyall <lyall@iriesoft.net>
2024-05-04 22:58:50 +0200

    veeif composable

--- src/vue-vif/composables/useVeeIfHelper.ts
+++ src/vue-vif/composables/useVeeIfHelper.ts
@@ -5,92 +5,95 @@ let schemaId = 0;
 
 // helper functions
 const removeChildren = (o: ISchema) => {
-	if (o.children?.length) o.children = undefined;
-	return o;
+  if (o.children?.length) o.children = undefined;
+  return o;
 };
 
 // main functions
 const makeFlat = (
-	data: object,
-	schema: ISchema[],
-	parentId: number | null = null,
+  data: object,
+  schema: ISchema[],
+  parentId: number | null = null
 ) => {
-	let result: ISchema[] = [];
+  let result: ISchema[] = [];
 
-	for (const s of schema) {
-		const rule = s.vif;
-		const logicResult = apply(rule, data);
+  for (const s of schema) {
+    const rule = s.vif;
+    const logicResult = apply(rule, data);
 
-		s.visible = logicResult;
+    s.visible = logicResult;
 
-		result.push({
-			...s,
-			id: ++schemaId,
-			parent: parentId,
-		});
+    result.push({
+      ...s,
+      id: ++schemaId,
+      parent: parentId,
+    });
 
-		if (Array.isArray(s.children)) {
-			result = result.concat(makeFlat(data, s.children, schemaId));
-		}
-	}
+    if (Array.isArray(s.children)) {
+      result = result.concat(makeFlat(data, s.children, schemaId));
+    }
+  }
 
-	return result;
+  return result;
 };
 
 const flatten = (d: object, s: ISchema[]) => {
-
-	schemaId = 0;
-	const f = makeFlat(d, s)
-	const o = orphan(f);
-	return o;
+  schemaId = 0;
+  const f = makeFlat(d, s);
+  const o = orphan(f);
+  return o;
 };
 
 const orphan = (flatSchema: ISchema[]) => {
-	const result = flatSchema.map((value, index, array) => {
-		return removeChildren(value);
-	});
+  const result = flatSchema.map((value, index, array) => {
+    return removeChildren(value);
+  });
 
-	return result;
+  return result;
 };
 
 const restructure = (flatSchema: ISchema[]) => {
-	const result: ISchema[] = [];
+  const result: ISchema[] = [];
 
-	for (const s of flatSchema) {
-		if (s.parent) {
-			const parent : any = deepSearchByKey(result, "id", s.parent)
+  for (const s of flatSchema) {
+    if (s.parent) {
+      const parent: any = deepSearchByKey(result, "id", s.parent);
 
-			if(parent.length) {
-				if (parent[0].children === undefined) parent[0].children = [];
-				parent[0]?.children?.push(s);
-			}
+      if (parent.length) {
+        if (parent[0].children === undefined) parent[0].children = [];
+        parent[0]?.children?.push(s);
+      }
+    } else {
+      result.push(s);
+    }
+  }
 
-		} else {
-			result.push(s);
-		}
-	}
-
-	return result;
+  return result;
 };
 
-const deepSearchByKey = (object: any, originalKey: string, originalValue: any, matches = []) => {
-	if (object != null) {
-		if (Array.isArray(object)) {
-			for (const arrayItem of object) {
-				deepSearchByKey(arrayItem, originalKey, originalValue, matches);
-			}
-		} else if (typeof object === 'object') {
-			for (const key of Object.keys(object)) {
-				if (key === originalKey) {
-					if (object[key] === originalValue) {
-						matches.push(object);
-					}
-				}
-				deepSearchByKey(object[key], originalKey, originalValue, matches);
-			}
-		}
-	}
-	return matches;
-}
+const deepSearchByKey = (
+  object: any,
+  originalKey: string,
+  originalValue: any,
+  matches: any[] = []
+) => {
+  if (object != null) {
+    if (Array.isArray(object)) {
+      for (const arrayItem of object) {
+        deepSearchByKey(arrayItem, originalKey, originalValue, matches);
+      }
+    } else if (typeof object === "object") {
+      for (const key of Object.keys(object)) {
+        if (key === originalKey) {
+          if (object[key] === originalValue) {
+            matches.push(object);
+          }
+        }
+        deepSearchByKey(object[key], originalKey, originalValue, matches);
+      }
+    }
+  }
+  return matches;
+};
 
 export { flatten, restructure };
